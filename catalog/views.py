9884601c247cc11661d4cea3d63d36f90425e498
omercelikors <omercelikors.softdev@gmail.com>
2021-07-24 20:32:22 +0300

    bidding history is added to detail page

--- catalog/views.py
+++ catalog/views.py
@@ -6,7 +6,6 @@ from django.templatetags.static import static
 from django.urls import reverse
 from django.db.models import Q, When, Case
 import sys
-from django.forms.models import model_to_dict
 
 # Create your views here.
 def show_items(request):
@@ -91,15 +90,20 @@ def api_get_item(request):
 		else:
 			auto_bidding_status = 0
 		
-		item_bid_amounts = ItemUserBidAmount.objects.filter(item=item['id']).values()
 		prefix = 'https://' if request.is_secure() else 'http://'
 		item['image_path'] = prefix + request.get_host() + static(item['image_path'])
 		item['auction_start_date_time'] = item['auction_start_date_time'].strftime("%Y-%m-%d %H:%M:%S")
 		item['auction_finish_date_time'] = item['auction_finish_date_time'].strftime("%Y-%m-%d %H:%M:%S")
 		item['auto_bidding_status'] = auto_bidding_status
 
+		item_bid_amounts = ItemUserBidAmount.objects.filter(item=item['id']).order_by('-id').values()
+		for item_bid_amount in item_bid_amounts:
+			item_bid_amount['created_at'] = item_bid_amount['created_at'].strftime("%Y-%m-%d %H:%M:%S")
+
 		response_data = {}
-		response_data['result'] = item
+		response_data['result'] = {}
+		response_data['result']['item'] = item
+		response_data['result']['item_bid_amounts'] = list(item_bid_amounts)
 		response_data['message'] = "Get item is OK"
 		return JsonResponse(response_data, status=200)
 	else:
@@ -109,49 +113,83 @@ def api_get_item(request):
 
 def api_bid_now(request):
 	if request.is_ajax and request.method == "GET" and request.user.is_authenticated:
-		current_user = request.user
-		current_user_config = UserConfig.objects.get(user_id=current_user.id)
-		current_user_fund = current_user_config.fund
-		current_user_max_bid_amount = current_user_config.max_bid_amount
-
-		item_id = int(request.GET.get("item_id", None))
-		item = Item.objects.get(id=item_id)
-		item_last_price = item.last_auction_price if item.last_auction_price else item.min_price
-		item_auction_finish_date_time = item.auction_finish_date_time
-
-		item_user_bid_amount = ItemUserBidAmount.objects.filter(item_id=item_id).exists()
-		if item_user_bid_amount:
-			item_user_bid_amount = ItemUserBidAmount.objects.filter(item_id=item_id).last()
-			last_bid_user_id = item_user_bid_amount.user_id
-		else:
-			last_bid_user_id = None
-
+		item_id = request.GET.get("item_id", None)
+		# for current user
+		current_user_result = do_bid_actions(request.user.id, item_id)
 		response_data = {}
 		response_data['result'] = None
-		# time check
-		if item_auction_finish_date_time < datetime.now():
-			response_data['message'] = "Auction ended"
+		if current_user_result == True:
+			# run bot user if current user result is successful
+			bot_user_result = run_bot(request.user.id, item_id)
+			response_data['message'] = "Your bid has been received"
+			return JsonResponse(response_data, status=200)
+		else:
+			response_data['message'] = current_user_result
 			return JsonResponse(response_data, status=400)
-		# fund check
-		if current_user_fund < item_last_price+1:
-			response_data['message'] = "Insufficient balance"
-			return JsonResponse(response_data, status=400)
-		# last auctioneer check
-		if last_bid_user_id == current_user.id:
-			response_data['message'] = "The highest bid in the system belongs to you"
-			return JsonResponse(response_data, status=400)
-		# create item_user_bid_amount history
-		item_user_bid_amount = ItemUserBidAmount(user=current_user, item=item, bid_amount=item_last_price + 1, is_auto_bidding=0)
-		item_user_bid_amount.save()
-		# update user fund on user_config table
-		current_user_config.fund = current_user_fund - (item_last_price + 1)
-		current_user_config.save()
-		# update last_auction_price on item table
-		item.last_auction_price = item_last_price + 1
-		item.save()
 
-		response_data['message'] = "Your bid has been received"
-		return JsonResponse(response_data, status=200)
+def do_bid_actions(user_id, item_id, bot_user=False):
+	user = User.objects.get(pk=user_id)
+	user_config = UserConfig.objects.get(user_id=user_id)
+
+	item = Item.objects.get(id=item_id)
+	item_last_price = item.last_auction_price if item.last_auction_price else item.min_price
+
+	item_user_bid_amount = ItemUserBidAmount.objects.filter(item_id=item.id).exists()
+	if item_user_bid_amount:
+		item_user_bid_amount = ItemUserBidAmount.objects.filter(item_id=item.id).last()
+		last_bid_user_id = item_user_bid_amount.user_id
+	else:
+		last_bid_user_id = None
+
+	# max bid amount check for bot user
+	if bot_user:
+		if user_config.max_bid_amount < item_last_price + 1:
+			# user bot status will be zero in case of insufficient max bid amount
+			item_user_auto_bid_case = ItemUserAutoBidCase.objects.filter(user=user_id, item=item).first()
+			item_user_auto_bid_case.auto_bidding_status = 0
+			item_user_auto_bid_case.save()
+			message = "Insufficient max bid amount"
+			return message
+	# time check
+	if item.auction_finish_date_time < datetime.now():
+		message = "Auction ended"
+		return message
+
+	# fund check
+	if user_config.fund < item_last_price+1:
+		message = "Insufficient balance"
+		return message
+
+	# last auctioneer check
+	if last_bid_user_id == user_id:
+		message = "The highest bid in the system belongs to you"
+		return message
+
+	# create item_user_bid_amount history
+	if bot_user:
+		is_auto_bidding = 1
+	else:
+		is_auto_bidding = 0
+	item_user_bid_amount = ItemUserBidAmount(user=user, item=item, bid_amount=item_last_price + 1, is_auto_bidding=is_auto_bidding)
+	item_user_bid_amount.save()
+
+	# update user fund on user_config table
+	user_config.fund = (user_config.fund) - (item_last_price + 1)
+	if bot_user:
+		user_config.max_bid_amount = (user_config.max_bid_amount) - (item_last_price + 1)
+	user_config.save()
+
+	# update last_auction_price on item table
+	item.last_auction_price = item_last_price + 1
+	item.save()
+
+	return True
+
+def run_bot(current_user_id, item_id):
+	item_user_auto_bid_cases = ItemUserAutoBidCase.objects.filter(item_id=item_id, auto_bidding_status=1).exclude(user_id=current_user_id)
+	for item_user_auto_bid_case in item_user_auto_bid_cases:
+		do_bid_actions(item_user_auto_bid_case.user_id, item_id, bot_user=True)
+	return True
 
 def api_auto_bidding(request):
 	if request.is_ajax and request.method == "GET" and request.user.is_authenticated:
@@ -169,7 +207,7 @@ def api_auto_bidding(request):
 			# create
 			item_user_auto_bid_case = ItemUserAutoBidCase(user=current_user, item=item, auto_bidding_status=auto_bidding_status)
 			item_user_auto_bid_case.save()
-	
+
 		response_data = {}
 		response_data['result'] = True
 		response_data['message'] = "Auto bid update is OK"
